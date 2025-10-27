f5ba3b9c4a8e0a2aaff6a87655af351f2af7dbff
Arthur Felipe Recchia Espindola <149198356+felipe-recchia@users.noreply.github.com>
2025-10-27 10:19:26 -0300

    Update to use JSON - in progress

--- src/cadastro/README.md
+++
@@ -1,3 +0,0 @@
-# Cad_Usuarios
-PT-BR: Aplicação em Java que permite cadastrar, editar e excluir usuários.
-EN-US: Java Aplication that allows you to register, edit and delete users 
--- src/cadastro/model/Usuario.java
+++ src/cadastro/model/Usuario.java
@@ -12,13 +12,42 @@ public class Usuario {
     }
 
     public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
-        return "ID: " + id + ", Nome: " + nome + ", Email: " + email;
+        return "Usuário {id=" + id + ", nome='" + nome + "', email='" + email + "'}";
     }
 }
+
+
+// package cadastro.model;
+
+// public class Usuario {
+//     private int id;
+//     private String nome;
+//     private String email;
+
+//     public Usuario(int id, String nome, String email) {
+//         this.id = id;
+//         this.nome = nome;
+//         this.email = email;
+//     }
+
+//     public int getId() { return id; }
+//     public String getNome() { return nome; }
+//     public void setNome(String nome) { this.nome = nome; }
+//     public String getEmail() { return email; }
+//     public void setEmail(String email) { this.email = email; }
+
+//     @Override
+//     public String toString() {
+//         return "ID: " + id + ", Nome: " + nome + ", Email: " + email;
+//     }
+// }
